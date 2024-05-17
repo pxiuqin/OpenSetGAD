73cf804a6335ea1a13be719c04ec1746565f84ce
pattern_academy <xq.l@outlook.com>
2024-05-17 17:34:27 +0800

    Update train.py

--- train.py
+++ train.py
@@ -1,7 +1,7 @@
 # from domain_aware_prompt import DAPromptHead
 import joblib
 from graph_pro_prompt import GraphPro
-from graph_prompt import distance2center2, node_prompt_layer_feature_weighted_mean, node_prompt_layer_feature_weighted_sum, node_prompt_layer_linear_mean, node_prompt_layer_linear_sum, node_prompt_layer_sum, calc_loss, prompt_loss
+from graph_prompt import center_embedding, distance2center2, node_prompt_layer_feature_weighted_mean, node_prompt_layer_feature_weighted_sum, node_prompt_layer_linear_mean, node_prompt_layer_linear_sum, node_prompt_layer_sum, calc_loss, prompt_loss, prompt_loss_with_center
 from load_data import getdata
 from model_dynamic import GAT
 # from model import GAT
@@ -17,7 +17,7 @@ from utils import pairwise_sample
 from utils import edl_digamma_loss, relu_evidence, edl_mse_loss, edl_log_loss
 import torch.nn.functional as F
 from load_data import SocialDataset
-from gpf_prompt import SimplePrompt, GPFplusAtt, embeddings
+from gpf_prompt import CenterEmbedding, SimplePrompt, GPFplusAtt, embeddings
 
 INF = float("inf")
 
@@ -195,6 +195,10 @@ def extract_embeddings(g, model, num_all_samples, args, prompt = None, prompt_mo
             elif prompt == 'graphpro':
                 blocks[0].srcdata['features'] = prompt_model(blocks[0].srcdata['features'])
                 extract_features = model(blocks)
+            elif prompt == 'prompt':
+                blocks[0].srcdata['features'] = prompt_model.add(blocks[0].srcdata['features'])
+                extract_features = model(blocks)
+                # extract_features = prompt_model.add(extract_features)
             else:
                 extract_features = model(blocks)
 
@@ -540,6 +544,164 @@ def initial_train_with_prompt_loss(i, args, data_split, metrics, embedding_save_
         return model, label_center_emb
     else:
         return model
+
+def initial_train_with_prompt_loss_new(i, args, data_split, metrics, embedding_save_path, class_emb, model=None):
+    save_path_i, in_feats, num_isolated_nodes, g, labels, train_indices, validation_indices, test_indices, adjacency_matrix = getdata(
+        embedding_save_path, data_split, i, args)
+
+    if model is None:  # Construct the initial model
+        model = GAT(in_feats, args.hidden_dim, args.out_dim, args.num_heads, args.use_residual)
+    if args.use_cuda:
+        model.cuda()
+
+    prompt = CenterEmbedding(args.out_dim, len(set(labels.tolist())))
+
+    model_param_group = []
+    model_param_group.append({"params": model.parameters()})
+    model_param_group.append({"params": prompt.parameters()})
+
+    # print(model.parameters())
+    # Optimizer
+    optimizer = optim.Adam(params=model_param_group,lr=args.lr, weight_decay=1e-4)
+
+    # Start training
+    message = "\n------------ Start initial training ------------\n"
+    print(message)
+    with open(save_path_i + '/log.txt', 'a') as f:
+        f.write(message)
+    # record the highest validation nmi ever got for early stopping
+    best_vali_value = 1e-9
+    best_epoch = 0
+    wait = 0
+    # record validation nmi of all epochs before early stop
+    all_vali_value = []
+    # record the time spent in seconds on each batch of all training/maintaining epochs
+    seconds_train_batches = []
+    # record the time spent in mins on each epoch
+    mins_train_epochs = []
+    for epoch in range(args.n_epochs):
+        start_epoch = time.time()
+        losses = []
+        total_loss = 0
+        for metric in metrics:
+            metric.reset()
+
+        extract_features, extract_labels = extract_embeddings(g, model, len(labels), args)
+
+        sampler = dgl.dataloading.MultiLayerFullNeighborSampler(2)  # 两层图结构
+        dataloader = dgl.dataloading.DataLoader(
+            g, train_indices, sampler,
+            batch_size=args.batch_size,
+            shuffle=True,
+            drop_last=False,
+            )
+
+
+        for batch_id, (input_nodes, output_nodes, blocks) in enumerate(dataloader):
+            device = torch.device("cuda:{}".format(args.gpuid) if args.use_cuda else "cpu")
+            blocks = [b.to(device) for b in blocks]
+            batch_labels = blocks[-1].dstdata['labels']  # 最后一个块输出的标签
+
+            start_batch = time.time()
+            model.train()
+            prompt.train()
+            
+            # forward
+            pred = model(blocks)  # Representations of the sampled nodes (in the last layer of the NodeFlow).
+            c_embedding_prompt = prompt(pred, batch_labels)
+
+            loss,_pred = prompt_loss_with_center(pred, batch_labels, c_embedding_prompt, adjacency_matrix)
+            losses.append(loss.item())
+            total_loss += loss.item()
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            for metric in metrics:
+                metric(_pred, batch_labels, loss)
+
+            batch_seconds_spent = time.time() - start_batch
+            seconds_train_batches.append(batch_seconds_spent)
+            # end one batch
+
+        total_loss /= (batch_id + 1)
+        message = 'Epoch: {}/{}. Average loss: {:.4f}'.format(epoch + 1, args.n_epochs, total_loss)
+        for metric in metrics:
+            message += '\t{}: {:.4f}'.format(metric.name(), metric.value())
+        mins_spent = (time.time() - start_epoch) / 60
+        message += '\nThis epoch took {:.2f} mins'.format(mins_spent)
+        message += '\n'
+        print(message)
+        with open(save_path_i + '/log.txt', 'a') as f:
+            f.write(message)
+        mins_train_epochs.append(mins_spent)
+
+        extract_features, extract_labels = extract_embeddings(g, model, len(labels), args)
+        # np.save(save_path_i + '/features_' + str(epoch) + '.npy', extract_features)
+        # np.save(save_path_i + '/labels_' + str(epoch) + '.npy', extract_labels)
+
+        # 这里的值是NMI或者AMI
+        validation_value = evaluate(extract_features, extract_labels, validation_indices, epoch, num_isolated_nodes,
+                                  save_path_i, args, True)
+        all_vali_value.append(validation_value)
+
+        # Early stop
+        if validation_value > best_vali_value:
+            best_vali_value = validation_value
+            best_epoch = epoch
+            wait = 0
+            # Save model
+            model_path = save_path_i + '/models'
+            if (epoch == 0) and (not os.path.isdir(model_path)):
+                os.mkdir(model_path)
+            p = model_path + '/best.pt'
+            torch.save(model.state_dict(), p)
+
+        # else:
+        #     wait += 1
+        # if wait == args.patience:
+        #     print('Saved all_mins_spent')
+        #     print('Early stopping at epoch ', str(epoch))
+        #     print('Best model was at epoch ', str(best_epoch))
+        #     break
+        # end one epoch
+
+    # Save all validation nmi
+    # np.save(save_path_i + '/all_vali_value.npy', np.asarray(all_vali_value))
+    # Save time spent on epochs
+    # np.save(save_path_i + '/mins_train_epochs.npy', np.asarray(mins_train_epochs))
+    # print('Saved mins_train_epochs.')
+    # Save time spent on batches
+    # np.save(save_path_i + '/seconds_train_batches.npy', np.asarray(seconds_train_batches))
+    # print('Saved seconds_train_batches.')
+    # Load the best model of the current block
+    best_model_path = save_path_i + '/models/best.pt'
+    model.load_state_dict(torch.load(best_model_path))
+    print("Best model loaded.")
+
+
+    label_center = {}
+    with torch.no_grad():
+        prompt.eval()
+        c_embedding_prompt = prompt(torch.FloatTensor(extract_features), torch.LongTensor(extract_labels))
+
+    # 变量不同label，并计算该label对应的平均特征表示
+    for l in set(extract_labels):
+        # 采用求平均的方法
+        # l_indices = np.where(extract_labels == l)[0]
+        # l_feas = extract_features[l_indices]
+        # l_cen = np.mean(l_feas, 0)
+        # label_center[l] = l_cen
+
+        # 采用CenterEmbedding的方式
+        label_center[l] = c_embedding_prompt[l]
+
+    joblib.dump(label_center,save_path_i + '/models/label_center.dump')
+
+    # label_center_emb = torch.FloatTensor(np.array(list(label_center.values()))).cuda() if args.use_cuda else torch.FloatTensor(np.array(list(label_center.values())))
+    # torch.save(label_center_emb,save_path_i + '/models/center.pth')
+
+    return model
 
 
 def continue_train(i, data_split, metrics, embedding_save_path, loss_fn, model, label_center_emb, args):
@@ -1172,6 +1334,181 @@ def graphpro_prompt_train(i, data_split, metrics, embedding_save_path, model, la
 
         l_cen = np.mean(l_feas,0)
         label_center[l] = l_cen
+    # joblib.dump(label_center,save_path_i + '/models/label_center.dump')
+
+    # 对旧类占比求平均
+    old_label_rate = (old_label_rate + current_old_label_rate) / 2 
+    print(old_label_rate)
+    # # Save time spent on epochs
+    # np.save(save_path_i + '/mins_train_epochs.npy', np.asarray(mins_train_epochs))
+    # print('Saved mins_train_epochs.')
+    # # Save time spent on batches
+    # np.save(save_path_i + '/seconds_train_batches.npy', np.asarray(seconds_train_batches))
+    # print('Saved seconds_train_batches.')
+
+    return model,label_center,old_label_rate
+
+def graphpro_prompt_train_new(i, data_split, metrics, embedding_save_path, model, label_center, args, old_label_rate, score_result=[]):
+    save_path_i, in_feats, num_isolated_nodes, g, labels, test_indices, adjacency_matrix = getdata(
+        embedding_save_path, data_split, i, args)
+
+    # 训练之前先搞下评估，然后看看一个block完成后，评估的比较
+    extract_features, extract_labels = extract_embeddings(g, model, len(labels), args)
+    test_value = evaluate(extract_features, extract_labels, test_indices, -1, num_isolated_nodes,
+                            save_path_i, args, True)
+    
+    p_num = len(set(labels.tolist()))
+    n_prompt = GPFplusAtt(in_feats, p_num)   # args.out_dim
+    # n_prompt = GPFplusAtt(args.out_dim, p_num)
+    c_prompt = CenterEmbedding(args.out_dim, p_num)
+
+    model_param_group = []
+    model_param_group.append({"params": n_prompt.parameters()})
+    model_param_group.append({"params": c_prompt.parameters()})
+
+    # Optimizer
+    optimizer = optim.Adam(params=model_param_group, lr=args.lr, weight_decay=1e-4, amsgrad=False)
+
+    # Start prompt tuning
+    message = "\n------------ Start prompt tuning------------\n"
+    print(message)
+    with open(save_path_i + '/log.txt', 'a') as f:
+        f.write(message)
+
+    label_center_emb = torch.FloatTensor(np.array(list(label_center.values()))).cuda() if args.use_cuda else torch.FloatTensor(np.array(list(label_center.values())))
+    
+    # record the time spent in seconds on each batch of all training/maintaining epochs
+    seconds_train_batches = []
+    # record the time spent in mins on each epoch
+    mins_train_epochs = []
+    for epoch in range(args.prompt_epochs):
+        start_epoch = time.time()
+        losses = []
+        total_loss = 0
+        for metric in metrics:
+            metric.reset()
+
+        sampler = dgl.dataloading.MultiLayerFullNeighborSampler(2)
+        dataloader = dgl.dataloading.DataLoader(
+            g, test_indices, sampler,
+            batch_size=args.batch_size,
+            shuffle=True,
+            drop_last=False,
+            )
+
+        for batch_id, (input_nodes, output_nodes, blocks) in enumerate(dataloader):
+            device = torch.device("cuda:{}".format(args.gpuid) if args.use_cuda else "cpu")
+            blocks = [b.to(device) for b in blocks]
+            batch_labels = blocks[-1].dstdata['labels']
+
+            start_batch = time.time()
+            n_prompt.train()
+            c_prompt.train()
+
+            # forward
+            blocks[0].srcdata['features'] = n_prompt.add(blocks[0].srcdata['features'])
+            pred = model(blocks)  # Representations of the sampled nodes (in the last layer of the NodeFlow) add prompt vector
+            # pred = n_prompt.add(pred)
+
+            # 伪标签的Loss函数
+            # pred_norm = F.normalize(pred, 2, 1)
+            rela_center_vec = torch.mm(pred,label_center_emb.t())
+            rela_center_vec = F.normalize(rela_center_vec,2,1)
+            epsilon = 1e-10
+            rela_center_vec = torch.clamp(rela_center_vec, min=epsilon)
+            entropy = torch.mul(torch.log(rela_center_vec), rela_center_vec)
+            entropy = torch.sum(entropy,dim=1)
+            value,old_indices = torch.topk(entropy.reshape(-1),int(old_label_rate*entropy.shape[0]),largest=True)   # 最大熵的一半
+            # value,novel_indices = torch.topk(entropy.reshape(-1),int(entropy.shape[0]/2),largest=False)   # 最小熵的一半
+
+            # 模型预测结果，通过欧式距离判断属于已知类的那类
+            distance = distance2center2(pred, label_center_emb)
+            distance = 1/F.normalize(distance, dim=1)
+            label_pred = F.log_softmax(distance, dim=1)
+            label_pred = torch.argmax(label_pred, dim=1, keepdim=True).squeeze()  # 这里是判断所有预测node，属于label_center_emb的那个类别
+
+            # 开始构建一个样本，创建一个新类的标识数组
+            pseudo_new_label = len(label_center.keys())
+            pseudo_labels = torch.full((pred.shape[0],), pseudo_new_label)
+            pseudo_labels[old_indices] = label_pred[old_indices]
+            c_embedding_prompt = c_prompt(pred, pseudo_labels)
+            loss,_pred = prompt_loss_with_center(pred, pseudo_labels, c_embedding_prompt, adjacency_matrix)
+            losses.append(loss.item())
+            total_loss += loss.item()
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            # 验证 pseudo old label 是否准确
+            pseudo_old_labels = batch_labels[old_indices]
+            print('pseudo old event:',len(set(pseudo_old_labels.tolist())))
+            print('true old event:',len(label_center.keys()))
+            print(set(label_center.keys()) & set(pseudo_old_labels.tolist()))
+
+            # 构建一个 pseudo old&new label
+            true_labels = process_tensor(batch_labels, label_center.keys())
+            current_old_label_rate = torch.sum(true_labels == 0).item() / true_labels.shape[0]  # 计算当前旧类占比
+            true_labels[true_labels == 1] = pseudo_new_label
+            true_labels[old_indices] = label_pred[old_indices] 
+
+            for metric in metrics:
+                metric(_pred, true_labels, loss)   # combined_tensor
+
+            batch_seconds_spent = time.time() - start_batch
+            seconds_train_batches.append(batch_seconds_spent)
+            # end one batch
+
+        total_loss /= (batch_id + 1)
+        message = 'Epoch: {}/{}. Average loss: {:.4f}'.format(epoch + 1, args.prompt_epochs, total_loss)
+        for metric in metrics:
+            message += '\t{}: {:.4f}'.format(metric.name(), metric.value())
+        mins_spent = (time.time() - start_epoch) / 60
+        message += '\nThis epoch took {:.2f} mins'.format(mins_spent)
+        message += '\n'
+        print(message)
+        # with open(save_path_i + '/log.txt', 'a') as f:
+        #     f.write(message)
+        mins_train_epochs.append(mins_spent)
+
+        # extract_features, extract_labels = extract_embeddings(g, model, len(labels), args)
+        extract_features, extract_labels = extract_embeddings(g, model, len(labels), args, prompt='prompt', prompt_model=n_prompt)
+
+        # save_embeddings(extract_nids, extract_features, extract_labels, extract_train_tags, save_path_i, epoch)
+        test_value = evaluate(extract_features, extract_labels, test_indices, epoch, num_isolated_nodes,
+                                save_path_i, args, True)
+
+        # 比较分数
+        if test_value >= MAX_SCORE[str(i)]:
+            score_result.append(f' Epoch {epoch+1} : {test_value}')
+
+    # Save model
+    model_path = save_path_i + '/models'
+    if not os.path.isdir(model_path):
+        os.mkdir(model_path)
+    # p = model_path + '/finetune.pt'
+    # torch.save(model.state_dict(), p)
+    # print('finetune model saved after epoch ', str(epoch))
+
+    with torch.no_grad():
+        c_prompt.eval()
+        c_embedding_prompt = c_prompt(torch.FloatTensor(extract_features), torch.LongTensor(extract_labels))
+
+    # update & save label_center
+    for l in set(extract_labels):
+        # l_indices = np.where(extract_labels==l)[0]
+        # l_feas = extract_features[l_indices]
+
+        # # 通过判断历史的计算值，可以构建一个移动平均，使得平均值更加稳定
+        # if l in label_center:
+        #     label_center_expanded = np.expand_dims(label_center[l], axis=0)
+        #     l_feas = np.concatenate((l_feas, label_center_expanded))
+
+        # l_cen = np.mean(l_feas,0)
+        # label_center[l] = l_cen
+
+        # 使用CenterEmbedding
+        label_center[l] = c_embedding_prompt[l]
     # joblib.dump(label_center,save_path_i + '/models/label_center.dump')
 
     # 对旧类占比求平均
